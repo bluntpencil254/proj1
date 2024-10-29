d25aa6cf0cfc710b3c9f0dd1ccf28f2e3f5f78cc
bluntpencil254 <makorimike9@gmail.com>
2024-10-29 21:06:55 +0300

    final project build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react'
 function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  const addValue = () =>{
+    setCounter(counter + 1)
+    //  setCounter((prevCounter) => prevCounter + 1)
+    //  setCounter((prevCounter) => prevCounter + 1)
+    //  setCounter((prevCounter) => prevCounter + 1)
+    //  setCounter((prevCounter) => prevCounter + 1)
+
+
+  }
+
+  const remoValue = () =>{
+    if (counter > 0) {
+      setCounter(counter - 1)
+    //  setCounter((prevCounter)=> prevCounter - 4)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quick react course</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add Value</button>{' '}
+
+      <button onClick={remoValue}>Remove Value</button>
+      <p>Total is: {counter}</p>
+
     </div>
   );
 }
